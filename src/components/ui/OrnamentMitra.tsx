de2ea5ce1fe7dfc307fb4cc075c261f434244c43
DwiNaufalAprialdhi <dwinaufalaprialdhi@gmail.com>
2025-04-14 13:20:46 +0700

    first commit

--- src/components/ui/OrnamentMitra.tsx
+++ src/components/ui/OrnamentMitra.tsx
@@ -6,13 +6,13 @@ export default function OrnamentMitra() {
             <>
                   {/* Ornament */}
                   {/* overflow-x-hidden relative */}
-                  <div className="absolute top-[120px] left-0 w-full h-full z-[2]">
+                  <div className="absolute top-[100px] left-0 w-full h-full z-[2]">
                         <Image src="/assets/ornament/om-1.svg" alt="Ornament" width={1500} height={834} className="w-full h-max object-cover" />
                   </div>
-                  <div className="absolute top-[120px] left-0 w-full h-full z-[3]">
+                  <div className="absolute top-[100px] left-0 w-full h-full z-[3]">
                         <Image src="/assets/ornament/om-2.svg" alt="Ornament" width={1500} height={834} className="w-full h-max object-cover" />
                   </div>
-                  <div className="absolute top-[120px] left-0 w-full h-full z-[1]">
+                  <div className="absolute top-[100px] left-0 w-full h-full z-[1]">
                         <Image src="/assets/ornament/om-3.svg" alt="Ornament" width={1500} height={834} className="w-full h-max object-cover" />
                   </div>
                   {/* <div className="absolute top-[100px] left-0 w-full h-full z-[4]">
