dca2e86ae082ed4c259160fa15fc6294751e4313
DwiNaufalAprialdhi <dwinaufalaprialdhi@gmail.com>
2025-04-18 16:41:52 +0700

    first commit

--- src/components/ui/Statistics.tsx
+++ src/components/ui/Statistics.tsx
@@ -11,9 +11,9 @@ export default function Statistics(props: StatisticsProps) {
       return (
             <>
                   <div className='w-full col-span-1 flex flex-col lg:items-start lg:justify-start items-center justify-center'>
-                        <Icon name={props.name} className='w-6 h-6 mb-2 text-white' />
-                        <h2 className='font-medium lg:text-sm text-xs leading-[21px] text-white mb-1 text-center lg:text-start'>{props.title}</h2>
-                        <h2 className='font-semibold lg:text-[20px] text-base leading-[25px] text-white text-center lg:text-start'>{props.value}</h2>
+                        <Icon name={props.name} className='w-6 h-6 mb-2 lg:text-white text-theme-dark' />
+                        <h2 className='font-medium lg:text-sm text-xs leading-[21px] lg:text-white text-theme-secondary mb-1 text-center lg:text-start'>{props.title}</h2>
+                        <h2 className='font-semibold lg:text-[20px] text-base leading-[25px] lg:text-white text-theme-green text-center lg:text-start'>{props.value}</h2>
                   </div>
             </>
       )
