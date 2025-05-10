f6550c68403f42d4848dbb65b950e600472ffcfa
DwiNaufalAprialdhi <dwinaufalaprialdhi@gmail.com>
2025-05-10 10:54:08 +0700

    first commit

--- src/components/layouts/dashboard/LayoutDashboard.tsx
+++ src/components/layouts/dashboard/LayoutDashboard.tsx
@@ -10,7 +10,7 @@ export default function LayoutDashboard(props: Props) {
       return (
             <>
                   <main className='w-full h-auto bg-white'>
-                        <section className='w-full min-h-screen lg:max-w-[1440px] md:max-w-[696px] max-w-none mx-auto flex'>
+                        <section className='w-full min-h-screen flex'>
                               {/* Sidebar Left */}
                               <SidebarLeft />
                               {/* Main Content */}
